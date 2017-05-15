30cac118c6d67cbb0dc71a4091f20b599759790e
emrhgnc <ge.emrah@gmail.com>
2017-05-16 00:05:59 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var db = require('./db');
 
-
+var UserController = require('./Controllers/UserController');
+app.use('/', UserController);
 
 var ProjectController = require('./Controllers/ProjectController');
 app.use('/', ProjectController);
